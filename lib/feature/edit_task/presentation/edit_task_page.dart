8423a9f6dbd4700f88eaf113282bc0bd942c15ab
iwp10 <dikipro11@gmail.com>
2026-07-04 00:58:39 +0700

    feat: create application structure

---
+++ lib/feature/edit_task/presentation/edit_task_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditTaskPage extends StatelessWidget {
+  const EditTaskPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Edit Task')),
+      body: const Center(
+        child: Text('Edit task screen placeholder'),
+      ),
+    );
+  }
+}
