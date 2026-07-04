3745549f9171f32ad87ed74c233c795f30a0d9c8
iwp10 <dikipro11@gmail.com>
2026-07-04 22:09:31 +0700

    feat: setup material 3 theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:simple_todo/feature/splash/presentation/splash_page.dart';
+import 'package:simple_todo/shared/theme/app_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Simple Todo',
-      home: SplashPage(),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: const SplashPage(),
     );
   }
 }
---
+++ lib/shared/theme/app_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.dark,
+    ),
+  );
+}
